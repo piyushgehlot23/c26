4ae44abf39702b12b76fce6f2408b2d1ad98c067
piyushgehlot23 <piyushgehlot229@gmail.com>
2021-03-19 20:40:42 +0530

    k

--- sketch.js
+++ sketch.js
@@ -33,8 +33,9 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    //creaing a bird object
     bird = new Bird(100,100);
+
 
 }
 
